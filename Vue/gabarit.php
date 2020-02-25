5150eb90c486525e95a2c6f7fbbc74a196c7d2e9
KillianFournier <killianfournier0@gmail.com>
2020-02-25 23:29:39 +0100

    Début du front gabarit

--- Vue/gabarit.php
+++ Vue/gabarit.php
@@ -2,18 +2,53 @@
 <html lang="fr">
   <head>
     <meta charset="UTF-8" />
-    <link rel="stylesheet" href="style.css" />
+    <link rel="stylesheet" href="Contenu/style.css" />
+    <link href='https://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <title><?= $titre ?></title>   <!-- Élément spécifique -->
   </head>
   <body>
     <div id="global">
       <header>
-        <a href="index.php"><h1 id="titreCombi">Combi</h1></a>
-        <p>Bienvenu dans le catalogue des combis.</p>
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <a class="navbar-brand" href="#">LOGO</a>
+
+  <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
+    <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Accueil <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Link</a>
+    </ul>
+  </div>
+</nav>
       </header>
+      <div class="jumbotron">
+      <h2 class="titre">The Combi Brazil</h2>
+      
+      <div class="Iam">
+  <p>Vous cherchez</p>
+  <b>
+  <div class="innerIam">
+    Un combi<br /> 
+    Une pièce<br />
+    Un conseil
+    </div>
+    </b>
+    </div>
+    <br />
+        <button type="button" class="btn btn-warning">Catalogue</button>
+  </p>
+      </div>
+      <h2> Qui somme nous ?</h2>
       <div id="contenu">
         <?= $contenu ?>   <!-- Élément spécifique -->
       </div>
+      
       <footer id="piedBlog">
         Blog réalisé avec PHP, HTML5 et CSS.
       </footer>
